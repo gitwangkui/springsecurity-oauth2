50e8e21824764f3351977040734dc414e05b858b
wangkui <1272840327ws>
2020-06-19 00:13:33 +0800

    Initial commit

---
+++ oauth2-server/src/main/java/com/mayikt/AppOauth2Server.java
@@ -0,0 +1,13 @@
+package com.mayikt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 认证授权中心
+@SpringBootApplication
+public class AppOauth2Server {
+	public static void main(String[] args) {
+		SpringApplication.run(AppOauth2Server.class, args);
+	}
+
+}
---
+++ oauth2-server/src/main/java/com/mayikt/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.mayikt.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// 拦截所有请求,使用httpBasic方式登陆
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/**")
+				.fullyAuthenticated().and().formLogin();
+	}
+}
